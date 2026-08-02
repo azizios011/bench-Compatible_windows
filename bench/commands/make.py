0d88c41709e6e90bda491c7f9d8d2cedf36851f4
azizios011 <hdriaziz699@gmail.com>
2026-08-02 13:34:05 +0100

    feat: add HRMS support to bench init command and system utilities

--- bench/commands/make.py
+++ bench/commands/make.py
@@ -23,11 +23,14 @@ import click
 @click.option("--frappe-path", default=None, help="path to frappe repo")
 @click.option("--erpnext-path", default=None, help="path to erpnext repo")
 @click.option("--print-designer-path", default=None, help="path to print_designer repo")
+@click.option("--hrms-path", default=None, help="path to hrms repo")
 @click.option("--erpnext-branch", default=None, help="Clone a particular branch of erpnext")
 @click.option("--print-designer-branch", default=None, help="Clone a particular branch of print_designer")
+@click.option("--hrms-branch", default=None, help="Clone a particular branch of hrms")
 @click.option("--skip-frappe-clone", is_flag=True, default=False, help="Skip frappe clone if app exists")
 @click.option("--skip-erpnext-clone", is_flag=True, default=False, help="Skip erpnext clone if app exists")
 @click.option("--skip-print-designer-clone", is_flag=True, default=False, help="Skip print_designer clone if app exists")
+@click.option("--skip-hrms-clone", is_flag=True, default=False, help="Skip hrms clone if app exists")
 @click.option("--clone-from", default=None, help="copy repos from path")
 @click.option(
 	"--clone-without-update", is_flag=True, help="copy repos from path without update"
@@ -58,12 +61,15 @@ def init(
 	frappe_path,
 	erpnext_path,
 	print_designer_path,
+	hrms_path,
 	frappe_branch,
 	erpnext_branch,
 	print_designer_branch,
+	hrms_branch,
 	skip_frappe_clone,
 	skip_erpnext_clone,
 	skip_print_designer_clone,
+	skip_hrms_clone,
 	no_procfile,
 	no_backups,
 	clone_from,
@@ -94,12 +100,15 @@ def init(
 			frappe_path=frappe_path,
 			erpnext_path=erpnext_path,
 			print_designer_path=print_designer_path,
+			hrms_path=hrms_path,
 			frappe_branch=frappe_branch,
 			erpnext_branch=erpnext_branch,
 			print_designer_branch=print_designer_branch,
+			hrms_branch=hrms_branch,
 			skip_frappe_clone=skip_frappe_clone,
 			skip_erpnext_clone=skip_erpnext_clone,
 			skip_print_designer_clone=skip_print_designer_clone,
+			skip_hrms_clone=skip_hrms_clone,
 			install_app=install_app,
 			clone_from=clone_from,
 			skip_redis_config_generation=skip_redis_config_generation,
--- bench/utils/system.py
+++ bench/utils/system.py
@@ -29,9 +29,11 @@ def init(
 	frappe_path=None,
 	erpnext_path=None,
 	print_designer_path=None,
+	hrms_path=None,
 	frappe_branch=None,
 	erpnext_branch=None,
 	print_designer_branch=None,
+	hrms_branch=None,
 	verbose=False,
 	clone_from=None,
 	skip_redis_config_generation=False,
@@ -43,6 +45,7 @@ def init(
 	skip_frappe_clone=False,
 	skip_erpnext_clone=False,
 	skip_print_designer_clone=False,
+	skip_hrms_clone=False,
 ):
 	"""Initialize a new bench directory
 
@@ -128,6 +131,19 @@ def init(
 			get_app(
 				print_designer_path,
 				branch=print_designer_branch,
+				bench_path=path,
+				skip_assets=True,
+				verbose=verbose,
+				resolve_deps=False,
+			)
+
+		hrms_path = hrms_path or "https://github.com/azizios011/hrms_Compatible_windows.git"
+		hrms_branch = hrms_branch or default_branch
+		hrms_app_path = os.path.join(path, "apps", "hrms")
+		if not (skip_hrms_clone and os.path.lexists(hrms_app_path)):
+			get_app(
+				hrms_path,
+				branch=hrms_branch,
 				bench_path=path,
 				skip_assets=True,
 				verbose=verbose,
