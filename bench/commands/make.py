134b260a8f62218bf96f4d49a8905c2bee400536
azizios011 <hdriaziz699@gmail.com>
2026-08-12 19:34:59 +0100

    feat: add payments support to bench init command and system utilities

--- bench/commands/make.py
+++ bench/commands/make.py
@@ -24,15 +24,18 @@ import click
 @click.option("--erpnext-path", default=None, help="path to erpnext repo")
 @click.option("--print-designer-path", default=None, help="path to print_designer repo")
 @click.option("--hrms-path", default=None, help="path to hrms repo")
+@click.option("--payments-path", default=None, help="path to payments repo")
 @click.option("--webshop-path", default=None, help="path to webshop repo")
 @click.option("--erpnext-branch", default=None, help="Clone a particular branch of erpnext")
 @click.option("--print-designer-branch", default=None, help="Clone a particular branch of print_designer")
 @click.option("--hrms-branch", default=None, help="Clone a particular branch of hrms")
+@click.option("--payments-branch", default=None, help="Clone a particular branch of payments")
 @click.option("--webshop-branch", default=None, help="Clone a particular branch of webshop")
 @click.option("--skip-frappe-clone", is_flag=True, default=False, help="Skip frappe clone if app exists")
 @click.option("--skip-erpnext-clone", is_flag=True, default=False, help="Skip erpnext clone if app exists")
 @click.option("--skip-print-designer-clone", is_flag=True, default=False, help="Skip print_designer clone if app exists")
 @click.option("--skip-hrms-clone", is_flag=True, default=False, help="Skip hrms clone if app exists")
+@click.option("--skip-payments-clone", is_flag=True, default=False, help="Skip payments clone if app exists")
 @click.option("--skip-webshop-clone", is_flag=True, default=False, help="Skip webshop clone if app exists")
 @click.option("--clone-from", default=None, help="copy repos from path")
 @click.option(
@@ -65,16 +68,19 @@ def init(
 	erpnext_path,
 	print_designer_path,
 	hrms_path,
+	payments_path,
 	webshop_path,
 	frappe_branch,
 	erpnext_branch,
 	print_designer_branch,
 	hrms_branch,
+	payments_branch,
 	webshop_branch,
 	skip_frappe_clone,
 	skip_erpnext_clone,
 	skip_print_designer_clone,
 	skip_hrms_clone,
+	skip_payments_clone,
 	skip_webshop_clone,
 	no_procfile,
 	no_backups,
@@ -107,16 +113,19 @@ def init(
 			erpnext_path=erpnext_path,
 			print_designer_path=print_designer_path,
 			hrms_path=hrms_path,
+			payments_path=payments_path,
 			webshop_path=webshop_path,
 			frappe_branch=frappe_branch,
 			erpnext_branch=erpnext_branch,
 			print_designer_branch=print_designer_branch,
 			hrms_branch=hrms_branch,
+			payments_branch=payments_branch,
 			webshop_branch=webshop_branch,
 			skip_frappe_clone=skip_frappe_clone,
 			skip_erpnext_clone=skip_erpnext_clone,
 			skip_print_designer_clone=skip_print_designer_clone,
 			skip_hrms_clone=skip_hrms_clone,
+			skip_payments_clone=skip_payments_clone,
 			skip_webshop_clone=skip_webshop_clone,
 			install_app=install_app,
 			clone_from=clone_from,
--- bench/utils/system.py
+++ bench/utils/system.py
@@ -30,11 +30,13 @@ def init(
 	erpnext_path=None,
 	print_designer_path=None,
 	hrms_path=None,
+	payments_path=None,
 	webshop_path=None,
 	frappe_branch=None,
 	erpnext_branch=None,
 	print_designer_branch=None,
 	hrms_branch=None,
+	payments_branch=None,
 	webshop_branch=None,
 	verbose=False,
 	clone_from=None,
@@ -48,6 +50,7 @@ def init(
 	skip_erpnext_clone=False,
 	skip_print_designer_clone=False,
 	skip_hrms_clone=False,
+	skip_payments_clone=False,
 	skip_webshop_clone=False,
 ):
 	"""Initialize a new bench directory
@@ -147,6 +150,19 @@ def init(
 			get_app(
 				hrms_path,
 				branch=hrms_branch,
+				bench_path=path,
+				skip_assets=True,
+				verbose=verbose,
+				resolve_deps=False,
+			)
+
+		payments_path = payments_path or "https://github.com/azizios011/payments-Compatible_windows.git"
+		payments_branch = payments_branch or default_branch
+		payments_app_path = os.path.join(path, "apps", "payments")
+		if not (skip_payments_clone and os.path.lexists(payments_app_path)):
+			get_app(
+				payments_path,
+				branch=payments_branch,
 				bench_path=path,
 				skip_assets=True,
 				verbose=verbose,
